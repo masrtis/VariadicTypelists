e7ecea7e25be6cd98db659e342aa1cc39293955f
Michael Sharpe <masrtis@gmail.com>
2013-07-15 19:48:21 -0700

    Refactored existing unit tests to use TypeList typedef.

--- VariadicTypelists/main.cpp
+++ VariadicTypelists/main.cpp
@@ -3,11 +3,15 @@
 
 int main()
 {
-	static_assert(TypeList<int, double, char>::Length == 3, "Incorrect number of typelist elements!");
-	static_assert(std::is_same<TypeAt<2, TypeList<int, double, char>>::Result, char>::value, "TypeAt<TypeList<int, double, char>, 2>::Result is not char!");
-	static_assert(IndexOf<float, TypeList<int, double, char>>::value == -1, "Found type that is not in typelist!");
-	static_assert(IndexOf<int, TypeList<int, double, char>>::value == 0, "Did not find int in TypeList<int, double, char>");
-	static_assert(IndexOf<char, TypeList<int, double, char>>::value == 2, "Did not find char in TypeList<int, double, char>");
+	typedef TypeList<int, double, char> Types;
+	
+	static_assert(Types::Length == 3, "Incorrect number of typelist elements!");
+
+	static_assert(std::is_same<TypeAt<2, Types>::Result, char>::value, "TypeAt<TypeList<int, double, char>, 2>::Result is not char!");
+
+	static_assert(IndexOf<float, Types>::value == -1, "Found type that is not in typelist!");
+	static_assert(IndexOf<int, Types>::value == 0, "Did not find int in TypeList<int, double, char>");
+	static_assert(IndexOf<char, Types>::value == 2, "Did not find char in TypeList<int, double, char>");
 
 	return 0;
 }
