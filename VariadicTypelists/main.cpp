26efedf916adf28334a9fb4e138c6e7cea1c6484
Michael Sharpe <masrtis@gmail.com>
2013-07-15 19:48:32 -0700

    Added tests for Append and Erase.

--- VariadicTypelists/main.cpp
+++ VariadicTypelists/main.cpp
@@ -13,5 +13,12 @@ int main()
 	static_assert(IndexOf<int, Types>::value == 0, "Did not find int in TypeList<int, double, char>");
 	static_assert(IndexOf<char, Types>::value == 2, "Did not find char in TypeList<int, double, char>");
 
+	static_assert(Append<int, TypeList<>>::Result::Length == 1, "Did not append int to empty typelist");
+	static_assert(Append<float, Types>::Result::Length == 4, "Did not append float to TypeList<int, double, char>");
+	static_assert(Append<Types, TypeList<float, double>>::Result::Length == 5, "Did not append TypeList<char, short, int> to TypeList<float, double>");
+
+	static_assert(Erase<int, Types>::Result::Length == 2, "Did not erase int from TypeList<int, double, char>");
+	static_assert(Erase<double, Types>::Result::Length == 2, "Did not erase double from TypeList<int, double, char>");
+
 	return 0;
 }
